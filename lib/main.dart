e082da45290245570a75bdb68bcc4ff44b4075e0
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2024-07-15 20:48:16 +0900

    FlutterとFirebaseを連携　iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:photoapp/sign_in_screen.dart';
 
-void main() {
+void main () async {
+  // Flutterの初期化処理を待つ
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // アプリ起動前にFirebase初期化処理を入れる
+  //   initializeApp()の返り値がFutureなので非同期処理
+  //   非同期処理(Future)はawaitで処理が終わるのを待つことができる
+  //   ただし、awaitを使うときは関数にasyncを付ける必要がある
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
